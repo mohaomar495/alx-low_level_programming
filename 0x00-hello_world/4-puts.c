57e4feb7a82afc0666aad18f33f751d8cd615f70
Mohammad Omar <mohaomar495@gmail.com>
2022-06-02 14:59:28 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
-
-puts("\"Hello world");
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts(“\”Hello, Puts”);
 return (0);
+}
