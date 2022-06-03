4273b1b3fb19daba5b307c373e05286147dc9194
Mohammad Omar <mohaomar495@gmail.com>
2022-06-03 14:38:03 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,19 @@
 
 /**
 * main - prints int
-* Description: n.o are under 10 without using any char vars
+* description: n.o are under 10 without using any char vars
 *
 * Return: 0
 */
 int main(void)
 {
-	int x = 48;
+int x = 48;
 
-	while (x < 58)
-	{
-		putchar(x);
-		x++;
-	}
+while (x < 58)
+{
+putchar(x);
+x++;
+}
 
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
