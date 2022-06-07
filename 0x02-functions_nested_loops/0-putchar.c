171b9b1f69d89598d18be9ed0a9eb49937f07c71
Mohammad Omar <mohaomar495@gmail.com>
2022-06-07 12:08:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+/**
+* main - prints a string
+* Description: the string is "_putchar"
+*
+* Return: 0
+*/
+int main(void)
+{
+	_putchar('_')
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
