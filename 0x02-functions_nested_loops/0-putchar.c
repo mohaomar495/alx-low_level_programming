67177cd473f26cc873f98d7acc3a2808eaa5c4c0
Mohammad Omar <mohaomar495@gmail.com>
2022-06-07 15:05:34 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
-
 /**
-* main - prints _putchar, followed by a new line
-* Return: Always 0 (Success)
+* main - prints a string
+* Description: the string is "Holberton"
+*
+* Return: 0
 */
 int main(void)
 {
-    _putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -15,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
-}
