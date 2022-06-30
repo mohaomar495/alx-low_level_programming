ed0bc9f0687740dea6752e0246881478f27d84bd
Mohammad Omar <mohaomar495@gmail.com>
2022-06-30 01:15:28 -0700

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,20 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
 
-/*
- * Numbers must be separated by , followed by space.
- * The two digits must be different.
- * 01 and 10 are considered the same combination of two digits 0 and 1.
- * Print only the smallest combinations of the two digits.
- *Numbers should be printed in ascending order.
- * Main - main block.
- *Description: print all combinations of two digits.
-*/
+/**
+ * main - main block
+ * Description: Print all possible combinations of two digits.
+ * Numbers must be separated by commas and a space.
+ * 01 and 10 are considered as the same combination of the two digits.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
+ * You can only use `putchar`.
+ * You can only use `putchar` up to 5 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
 
 int main(void)
 {
-
 	int n, m;
 
 	for (n = 48; n < 58; n++)
@@ -22,13 +23,17 @@ int main(void)
 		{
 			if (m > n)
 			{
-				putchar(m);
 				putchar(n);
+				putchar(m);
+				if (n < 56 || m < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
-			putchar(44);
-			putchar(32);
 		}
 	}
+	putchar('\n');
 
 	return (0);
 }
