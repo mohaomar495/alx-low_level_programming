9042a81b7cc4ce520033632dd3bcf4b8fe499e89
Mohammad Adde <mohaomar495@gmail.com>
2023-12-04 14:38:45 -0800

    C is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints something into the screen.
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
